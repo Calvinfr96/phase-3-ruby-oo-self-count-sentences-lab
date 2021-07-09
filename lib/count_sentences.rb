3fab3e7273fefa43ab7ce20a1314ba4219cbfa4c
calvin <calvin.francis96@outlook.com>
2021-07-09 14:18:17 -0400

    Done with assignment.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
-  def exclamation?
-
+  def exclamation?    
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    all_sentences = self.split(/\.|\?|\!/)
+    true_sentences = all_sentences.delete_if do |word|
+      word.size < 2
+    end
+    all_sentences.size
   end
 end
